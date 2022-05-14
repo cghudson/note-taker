652968d290a0c18bd2b1141d18863aaee790d847
Carolyn Hudson <96785153+cghudson@users.noreply.github.com>
2022-05-14 15:42:38 -0500

    modified delete

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const fs = require("fs");
-var uniqid = require('uniqid')
+var uniqid = require("uniqid");
 let data = require("../../db/db.json");
 
 router.get("/notes", (req, res) => {
@@ -14,20 +14,37 @@ router.post("/notes", (req, res) => {
     text: req.body.text,
     id: uniqid(),
   };
+
+  // think about using a spread operator for the data. 
+
   data.push(newNote);
+  // data = [...data, newNote];
+
   fs.writeFileSync("./db/db.json", JSON.stringify(data));
   res.json(data);
 });
 
 router.delete("/notes/:id", (req, res) => {
-  let save = [];
-  for (var i = 0; i < data.length; i++) {
-    if (data[i].id != req.params.id) {
-      save.push(data[i]);
-    }
-  }
-  data = save;
-  fs.writeFileSync("./db/db.json", JSON.stringify(data));
+  // let save = [];
+  // for (var i = 0; i < data.length; i++) {
+  //   if (data[i].id != req.params.id) {
+  //     save.push(data[i]);
+  //   }
+  // }
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+
+  // console.log(notes)
+  let filtered = notes
+    .filter((note) => note.id !== req.params.id)
+
+ 
+    // .then((filtered) =>
+      fs.writeFileSync("./db/db.json", JSON.stringify(filtered))
+    // )
+    // .then((data) => res.json(data));
+
+  // data = save;
+
   res.json(data);
 });
 
