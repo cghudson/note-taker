ce9410e4a26e4126fab70e653b4ac3cb30077b4c
Carolyn Hudson <96785153+cghudson@users.noreply.github.com>
2022-05-14 14:53:38 -0500

    delete comments, add uniqid npm

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,26 +1,24 @@
 const router = require("express").Router();
 const fs = require("fs");
+var uniqid = require('uniqid')
 let data = require("../../db/db.json");
 
-//GET
 router.get("/notes", (req, res) => {
   data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
   res.json(data);
 });
 
-//POST
 router.post("/notes", (req, res) => {
   let newNote = {
     title: req.body.title,
     text: req.body.text,
-    id: Math.floor(Math.random() * 100),
+    id: uniqid(),
   };
   data.push(newNote);
   fs.writeFileSync("./db/db.json", JSON.stringify(data));
   res.json(data);
 });
 
-//DELETE
 router.delete("/notes/:id", (req, res) => {
   let save = [];
   for (var i = 0; i < data.length; i++) {
